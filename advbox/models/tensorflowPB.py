03296d1e6cd9463fef80e7f9f2fe5fc86b19e0d0
maidou <250261953@qq.com>
2018-09-12 18:33:29 +0800

    add tempfile

---
+++ advbox/models/tensorflowPB.py
@@ -0,0 +1,134 @@
+#coding=utf-8
+# Copyright 2017 - 2018 Baidu Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Tensorflow model
+"""
+from __future__ import absolute_import
+
+import numpy as np
+import os
+
+from .base import Model
+
+import logging
+logger=logging.getLogger(__name__)
+
+#直接加载pb文件
+class TensorflowPBModel(Model):
+
+
+    def __init__(self,
+                 session,
+                 input,
+                 label,
+                 logits,
+                 loss,
+                 bounds,
+                 channel_axis=3,
+                 preprocess=None):
+
+        import tensorflow as tf
+
+
+        if preprocess is None:
+            preprocess = (0, 1)
+
+        super(TensorflowPBModel, self).__init__(
+            bounds=bounds, channel_axis=channel_axis, preprocess=preprocess)
+
+
+        self._session = session
+
+        self._loss=loss
+
+
+        self._label=label
+        self._logits=logits
+        self._input = input
+        self._input_shape = tuple(self._input.get_shape()[1:])
+        self._nb_classes=int(self._logits.get_shape()[-1])
+
+        logger.info('self._input_shape:'+str(self._input_shape))
+
+        logger.info("init grads[{0}]...".format(self._nb_classes))
+
+        self._grads= tf.gradients(self._loss, self._input)[0]
+
+        print(self._grads)
+
+        self._grads= tf.gradients(self._logits, self._input)[0]
+
+        print(self._grads)
+
+        logger.info("Finish TensorflowPBModel init")
+
+
+    def predict(self, data):
+        """
+        Calculate the prediction of the data.
+        Args:
+            data(numpy.ndarray): input data with shape (size,
+            height, width, channels).
+        Return:
+            numpy.ndarray: predictions of the data with shape (batch_size,
+                num_of_classes).
+        """
+
+        import tensorflow as tf
+
+        fd = {self._input: data}
+
+        # Run prediction
+        predict = self._session.run(self._logits, feed_dict=fd)
+
+        return predict
+
+    def num_classes(self):
+        """
+            Calculate the number of classes of the output label.
+        Return:
+            int: the number of classes
+        """
+
+        return int(self._logits.get_shape()[-1])
+
+    def gradient(self, data, label):
+        """
+        Calculate the gradient of the cross-entropy loss w.r.t the image.
+        Args:
+            data(numpy.ndarray): input data with shape (size, height, width,
+            channels).
+            label(int): Label used to calculate the gradient.
+        Return:
+            numpy.ndarray: gradient of the cross-entropy loss w.r.t the image
+                with the shape (height, width, channel).
+        """
+        #scaled_data = self._process_input(data)
+
+        import tensorflow as tf
+
+        grads = self._session.run(self._grads, feed_dict={self._input: data,self._label:label})
+
+        grads = grads[None, ...]
+        grads = np.swapaxes(np.array(grads), 0, 1)
+
+        return grads.reshape(data.shape)
+
+    def predict_name(self):
+        """
+        Get the predict name, such as "softmax",etc.
+        :return: string
+        """
+        return self._predict_program.block(0).var(self._predict_name).op.type
---
+++ tutorials/imagenet_tutorial_fgsm_tf.py
@@ -0,0 +1,138 @@
+#coding=utf-8
+
+# Copyright 2017 - 2018 Baidu Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+FGSM tutorial on mnist using advbox tool.
+FGSM method is non-targeted attack while FGSMT is targeted attack.
+"""
+import sys
+sys.path.append("..")
+import logging
+logging.basicConfig(level=logging.INFO,format="%(filename)s[line:%(lineno)d] %(levelname)s %(message)s")
+logger=logging.getLogger(__name__)
+
+#import matplotlib.pyplot as plt
+import numpy as np
+
+from advbox.adversary import Adversary
+from advbox.attacks.gradient_method import FGSM
+from advbox.models.tensorflowPB import TensorflowPBModel
+from tutorials.mnist_model_tf import mnist_cnn_model
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+def main(dirname,imagename):
+    """
+    Advbox demo which demonstrate how to use advbox.
+    """
+
+    image_data = tf.gfile.FastGFile(imagename, 'rb').read()
+
+
+    session=tf.Session()
+
+    def create_graph(dirname):
+        with tf.gfile.FastGFile(dirname, 'rb') as f:
+            graph_def = session.graph_def
+            graph_def.ParseFromString(f.read())
+
+            _ = tf.import_graph_def(graph_def, name='')
+
+    create_graph(dirname)
+
+    # 初始化参数  非常重要
+    session.run(tf.global_variables_initializer())
+
+    #tensorlist=[n.name for n in session.graph_def.node]
+
+    #logger.info(tensorlist)
+    #获取softmax层而非logit层
+    softmax = session.graph.get_tensor_by_name('softmax:0')
+
+    #获取softmax/logits
+    logits=session.graph.get_tensor_by_name('softmax/logits:0')
+
+    x = session.graph.get_tensor_by_name('DecodeJpeg/contents:0')
+
+    y = tf.placeholder(tf.int64, None, name='label')
+
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
+            labels=y, logits=logits)
+
+    #tf.gradients(tf.nn.softmax(self._logits)[:, label], self._input)[0]
+
+
+
+
+
+    print('!!!!!!!')
+    #print(logits[:, 0])
+    #print(tf.nn.softmax(logits[:, 0]) )
+    #print(x)
+    #print(cross_entropy)
+    #print(g)
+    #print(logits)
+    #print(softmax)
+    #logits = session.run(logits, feed_dict={x: image_data})
+    #print(logits)
+    #tf.gradients(tf.nn.softmax(self._logits)[:, label], self._input_ph)[0]
+    g = tf.gradients(logits[:, 1], x)[0]
+    print(g)
+
+
+    grads = session.run(g, feed_dict={x: image_data})
+
+    print(grads)
+
+
+
+
+    # advbox demo
+    m = TensorflowPBModel(
+        session,
+        x,
+        y,
+        softmax,
+        cross_entropy,
+        (0, 1),
+        channel_axis=1)
+
+    attack = FGSM(m)
+    attack_config = {"epsilons": 0.3}
+
+
+    #print(x.shape)
+    #print(y.shape)
+
+    adversary = Adversary(image_data,None)
+
+    # FGSM non-targeted attack
+    adversary = attack(adversary, **attack_config)
+
+
+    if adversary.is_successful():
+        print(
+            'attack success, adversarial_label=%d'
+            % (adversary.adversarial_label) )
+
+
+    print("fgsm attack done")
+
+
+if __name__ == '__main__':
+    #从'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'下载并解压到当前路径
+    #classify_image_graph_def.pb cropped_panda.jpg
+    main(dirname="classify_image_graph_def.pb",imagename="cropped_panda.jpg")
